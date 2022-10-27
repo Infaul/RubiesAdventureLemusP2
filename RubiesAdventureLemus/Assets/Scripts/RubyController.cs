c3da5558754f90c48636b99ffbf2a1764f072c4c
Eduardo L <714257@student.dusd.net>
2022-10-27 09:56:29 -0700

    Added movment
    coded Rubie to have x and y movements

--- RubiesAdventureLemus/Assets/Scripts/RubyController.cs
+++ RubiesAdventureLemus/Assets/Scripts/RubyController.cs
@@ -7,16 +7,17 @@ public class RubyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        Debug.Log(horizontal);
+        float vertical = Input.GetAxis("Vertical");
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal;
+        position.x = position.x + 0.3f * horizontal * Time.deltaTime;
+        position.y = position.y + 0.3f * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
